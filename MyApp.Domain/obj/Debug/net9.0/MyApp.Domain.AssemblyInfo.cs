df6aeab4b6e77c00e6f1887b6c2e079312f2d4f9
joaovitorangelo <joaovitorangelo05@gmail.com>
2025-07-30 22:35:39 -0300

    fix(infrastructure): corrigindo bugs

--- MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
+++ MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c114bc646abea77515c8a87e8e872adb5cd687")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
